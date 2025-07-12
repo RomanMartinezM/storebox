0b675085ad2a336dc84687236607396598f60e8b
ronmar <sigma4726spring@gmail.com>
2025-07-11 21:33:36 -0600

    feat: User model created

---
+++ src/main/java/myproject/storebox/StoreboxApplication.java
@@ -0,0 +1,13 @@
+package myproject.storebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreboxApplication.class, args);
+	}
+
+}
---
+++ src/main/java/myproject/storebox/model/User.java
@@ -0,0 +1,48 @@
+package myproject.storebox.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+@Where(clause = "deleted = false")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted = false;
+
+    // Soft delete method
+    public void softDelete() {
+        this.deleted = true;
+    }
+}
