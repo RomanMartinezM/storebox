b8bfd7b9defd8ad1618a956f66f0475448577ff7
ronmar <sigma4726spring@gmail.com>
2025-08-19 20:35:27 -0600

    feat: Security disabled

--- src/main/java/myproject/storebox/StoreboxApplication.java
+++ src/main/java/myproject/storebox/StoreboxApplication.java
@@ -3,8 +3,12 @@ package myproject.storebox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class
+})
 public class StoreboxApplication {
 
 	public static void main(String[] args) {
